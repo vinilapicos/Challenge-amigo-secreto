92cea195fc337eb430e7b2a72904859d4024392c
Vinicius Carvalho <vinilapicos@gmail.com>
2025-09-22 19:36:06 -0300

    implementar função que adiciona amigos a lista

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo;
+let listaAmigos = [];
+
+function adicionarAmigo(){
+        let amigo = document.getElementById('amigo').value;
+        if (amigo != ""){
+            listaAmigos.push(amigo);
+            document.getElementById('amigo').value = "";
+        }else {
+            alert("O campo não pode estar vazio!");
+        }
+}
