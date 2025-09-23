adb983b6fc3e1b2fa81ed1dddb6e48d45c60ef80
Vinicius Carvalho <vinilapicos@gmail.com>
2025-09-22 21:15:20 -0300

    Implementar botão e função para exibir lista e limpar lista

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigo;
 let listaAmigos = [];
+let listaHtml = document.getElementById('listaAmigos');
+let botaoExibir = document.getElementById('exibirLista');
 
 function adicionarAmigo(){
         let amigo = document.getElementById('amigo').value;
         if (amigo != ""){
             listaAmigos.push(amigo);
             document.getElementById('amigo').value = "";
+            botaoExibir.removeAttribute('disabled');
         }else {
             alert("O campo não pode estar vazio!");
         }
+}
+
+function exibirLista(){
+    listaHtml.innerHTML = '';
+    for (let i = 0; i < listaAmigos.length; i++){
+        let novoAmigo = document.createElement('li');
+        novoAmigo.textContent = listaAmigos[i];
+        listaHtml.appendChild(novoAmigo);
+    }
+}
+
+function limparLista(){
+    listaHtml.innerHTML = '';
+    listaAmigos = [];
+    botaoExibir.setAttribute('disabled', 'true');
 }
